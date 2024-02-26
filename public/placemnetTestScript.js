eccb52ca7a3daaf2d8b65f4d1518c8b6daf081d0
ananasdenisovich <113703176+ananasdenisovich@users.noreply.github.com>
2024-02-26 21:19:52 +0600

    Delete public directory

--- public/placemnetTestScript.js
+++
@@ -1,7 +0,0 @@
-function startPlacementTest(language) {
-  window.location.href = 'test.html?language=' + language;
-}//a
-
-function submitPlacementTest() {
-  alert('Placement test submitted!');
-}
